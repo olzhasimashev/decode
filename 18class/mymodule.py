aa2642399d40860016298c8fb0d9dd4dabc4f1c5
unknown <imashev.olzhas@gmail.com>
2023-02-19 19:27:32 +0600

    mymodule.py modified for hard b

--- 18class/mymodule.py
+++ 18class/mymodule.py
@@ -9,3 +9,13 @@ def reverse(word):
 
 def sum_index(values):
     return max(values)+min(values)
+
+def char_count(value):
+    d = {}
+    for i in value:
+        if i in d:
+            d[i] += 1
+        else:
+            d[i] = 1
+    for i in d:
+        print(i, ':', d[i])
