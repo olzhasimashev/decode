8e82caaf385c1dc1629b57a2197378114fec22c1
unknown <imashev.olzhas@gmail.com>
2023-03-02 14:49:23 +0600

    added 1b.py

---
+++ 22class, classes/1b.py
@@ -0,0 +1,16 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+x = int(input())
+y = int(input())
+
+object = Point(x, y)
+
+input = input()
+
+if input == "x":
+    print(object.x)
+elif input == "y":
+    print(object.y)
